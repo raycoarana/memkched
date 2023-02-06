6f94c2e99932da6b59be3d338be2da43d2e2af0f
rayco <me@raycoarana.com>
2023-02-06 23:09:43 +0100

    Ping memcached

--- src/test/kotlin/integration/com/raycoarana/memkched/HelloIntegrationTest.kt
+++ src/test/kotlin/integration/com/raycoarana/memkched/HelloIntegrationTest.kt
@@ -1,6 +1,15 @@
 package com.raycoarana.memkched
 
 import org.junit.jupiter.api.Test
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+import org.testcontainers.utility.DockerImageName
+import java.net.InetSocketAddress
+import java.net.SocketAddress
+import java.nio.ByteBuffer
+import java.nio.channels.AsynchronousSocketChannel
+import java.nio.channels.SocketChannel
 import kotlin.test.assertEquals
 
 @Testcontainers
@@ -12,6 +21,21 @@ class HelloIntegrationTest {
 
     @Test
     fun `sample test`() {
-        assertEquals("H", "H")
+        val channel = AsynchronousSocketChannel.open()
+        val future = channel.connect(InetSocketAddress(memcached.host, memcached.getMappedPort(11211)))
+        future.get()
+        val buffer = ByteBuffer.allocate(4096)
+        buffer.put("set HELLO 0 0 5\r\nMOLA!\r\n".toByteArray(Charsets.US_ASCII)).flip()
+        var writtenBytes = channel.write(buffer).get()
+        println("bytes send => $writtenBytes")
+        val getCmd = "get HELLO\r\n".toByteArray(Charsets.US_ASCII)
+        buffer.rewind().put(getCmd).flip()
+        writtenBytes = channel.write(buffer).get()
+        println("bytes send => $writtenBytes")
+
+        val readBytes = channel.read(buffer.rewind().limit(buffer.capacity())).get()
+        println("bytes read => $readBytes")
+        String(buffer.flip().array(), 0, readBytes).split("\r\n")
+            .forEach { println("line => \"$it\"") }
     }
 }
