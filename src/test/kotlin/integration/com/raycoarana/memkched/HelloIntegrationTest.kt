8209090b933d2d24adbba0675a27ed7099cce661
rayco <me@raycoarana.com>
2023-02-06 20:03:03 +0100

    Add test containers

--- src/test/kotlin/integration/com/raycoarana/memkched/HelloIntegrationTest.kt
+++ src/test/kotlin/integration/com/raycoarana/memkched/HelloIntegrationTest.kt
@@ -3,7 +3,13 @@ package com.raycoarana.memkched
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
+@Testcontainers
 class HelloIntegrationTest {
+
+    @Container
+    private val memcached = GenericContainer(DockerImageName.parse("memcached:1.6.18-alpine"))
+        .withExposedPorts(11211)
+
     @Test
     fun `sample test`() {
         assertEquals("H", "H")
