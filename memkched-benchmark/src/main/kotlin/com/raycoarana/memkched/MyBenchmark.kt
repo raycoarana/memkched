4d2d37011ac3ed76b28759bad7b75f52d420a3e9
raycoarana <me@raycoarana.com>
2023-02-27 20:06:37 +0100

    Add benchmark module

---
+++ memkched-benchmark/src/main/kotlin/com/raycoarana/memkched/MyBenchmark.kt
@@ -0,0 +1,11 @@
+package com.raycoarana.memkched
+
+import org.openjdk.jmh.annotations.Benchmark
+
+open class MyBenchmark {
+    @Benchmark
+    fun testMethod() {
+        // This is a demo/sample template for building your JMH benchmarks. Edit as needed.
+        // Put your benchmark code here.
+    }
+}
