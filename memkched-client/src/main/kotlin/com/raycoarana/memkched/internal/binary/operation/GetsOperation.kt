c7f4a9923e089b03cff30d6abdb58cabddd29340
raycoarana <me@raycoarana.com>
2023-03-26 18:05:05 +0200

    Abstract the get/multiget to implement gets/mutigets

--- memkched-client/src/main/kotlin/com/raycoarana/memkched/internal/binary/operation/GetsOperation.kt
+++ memkched-client/src/main/kotlin/com/raycoarana/memkched/internal/binary/operation/GetsOperation.kt
@@ -1,13 +1,15 @@
 package com.raycoarana.memkched.internal.binary.operation
 
-import com.raycoarana.memkched.internal.Operation
-import com.raycoarana.memkched.internal.binary.BinaryProtocolSocketChannelWrapper
+import com.raycoarana.memkched.api.CasUnique
+import com.raycoarana.memkched.api.Flags
+import com.raycoarana.memkched.internal.binary.model.OpCode.GET
 import com.raycoarana.memkched.internal.result.GetsGatsResult
+import com.raycoarana.memkched.internal.result.GetsGatsResult.NotFound
+import com.raycoarana.memkched.internal.result.GetsGatsResult.Value
 
 internal open class GetsOperation(
-    private val key: String
-) : Operation<BinaryProtocolSocketChannelWrapper, GetsGatsResult<ByteArray>>() {
-    override suspend fun run(socket: BinaryProtocolSocketChannelWrapper): GetsGatsResult<ByteArray> {
-        TODO("Not yet implemented")
-    }
+    key: String
+) : AbstractGetOperation<GetsGatsResult<ByteArray>>(GET, key) {
+    override fun mapToValue(flags: Flags, value: ByteArray, cas: CasUnique) = Value(flags, value, cas)
+    override fun mapToNotFound() = NotFound
 }
--- memkched-client/src/main/kotlin/com/raycoarana/memkched/internal/binary/operation/MultiGetsOperation.kt
+++ memkched-client/src/main/kotlin/com/raycoarana/memkched/internal/binary/operation/MultiGetsOperation.kt
@@ -1,15 +1,14 @@
 package com.raycoarana.memkched.internal.binary.operation
 
-import com.raycoarana.memkched.internal.Operation
-import com.raycoarana.memkched.internal.binary.BinaryProtocolSocketChannelWrapper
+import com.raycoarana.memkched.api.CasUnique
+import com.raycoarana.memkched.api.Flags
 import com.raycoarana.memkched.internal.result.GetsGatsResult
+import com.raycoarana.memkched.internal.result.GetsGatsResult.NotFound
+import com.raycoarana.memkched.internal.result.GetsGatsResult.Value
 
 internal open class MultiGetsOperation(
-    private val keys: List<String>
-) : Operation<BinaryProtocolSocketChannelWrapper, Map<String, GetsGatsResult<ByteArray>>>() {
-    override suspend fun run(
-        socket: BinaryProtocolSocketChannelWrapper
-    ): Map<String, GetsGatsResult<ByteArray>> {
-        TODO("Not yet implemented")
-    }
+    keys: List<String>
+) : AbstractMultiGetOperation<GetsGatsResult<ByteArray>>(keys) {
+    override fun mapToValue(flags: Flags, value: ByteArray, casUnique: CasUnique) = Value(flags, value, casUnique)
+    override fun mapToNotFound() = NotFound
 }
