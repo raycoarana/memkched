97f7ceea829f9f22247ec2e5eb2451d29d1e3b93
raycoarana <me@raycoarana.com>
2023-03-26 18:23:45 +0200

    Implement Cas/Add/Replace operations

--- memkched-client/src/main/kotlin/com/raycoarana/memkched/internal/binary/operation/AddOperation.kt
+++ memkched-client/src/main/kotlin/com/raycoarana/memkched/internal/binary/operation/AddOperation.kt
@@ -5,6 +5,9 @@ import com.raycoarana.memkched.api.Flags
 import com.raycoarana.memkched.api.Reply
 import com.raycoarana.memkched.internal.Operation
 import com.raycoarana.memkched.internal.binary.BinaryProtocolSocketChannelWrapper
+import com.raycoarana.memkched.internal.binary.buildExtrasWith
+import com.raycoarana.memkched.internal.binary.model.OpCode
+import com.raycoarana.memkched.internal.binary.model.Status.KEY_EXISTS
 import com.raycoarana.memkched.internal.result.AddReplaceResult
 
 internal class AddOperation(
@@ -15,6 +18,34 @@ internal class AddOperation(
     private val reply: Reply
 ) : Operation<BinaryProtocolSocketChannelWrapper, AddReplaceResult>() {
     override suspend fun run(socket: BinaryProtocolSocketChannelWrapper): AddReplaceResult {
-        TODO("Not yet implemented")
+        val opCode = if (reply == Reply.NO_REPLY) {
+            OpCode.ADDQ
+        } else {
+            OpCode.ADD
+        }
+        socket.writePackage(
+            opCode = opCode,
+            key = key,
+            extras = buildExtrasWith(flags, expiration),
+            body = data
+        )
+
+        return if (reply == Reply.DEFAULT) {
+            socket.readHeader(
+                headerProcess = { resultOpCode, _, _, _, _, _ ->
+                    assert(resultOpCode == OpCode.ADD) { "Unexpected op code in response $resultOpCode" }
+
+                    AddReplaceResult.Stored
+                },
+                errorProcess = { error, _ ->
+                    when (error.status) {
+                        KEY_EXISTS -> AddReplaceResult.NotStored
+                        else -> throw error.asException()
+                    }
+                }
+            )
+        } else {
+            AddReplaceResult.NoReply
+        }
     }
 }
--- memkched-client/src/test/kotlin/integration/com/raycoarana/memkched/MemkchedBinaryIntegrationTest.kt
+++ memkched-client/src/test/kotlin/integration/com/raycoarana/memkched/MemkchedBinaryIntegrationTest.kt
@@ -1,9 +1,13 @@
 package com.raycoarana.memkched
 
+import com.raycoarana.memkched.api.CasUnique
 import com.raycoarana.memkched.api.Expiration.Relative
 import com.raycoarana.memkched.api.Flags
 import com.raycoarana.memkched.api.Protocol.BINARY
+import com.raycoarana.memkched.internal.result.AddReplaceResult
+import com.raycoarana.memkched.internal.result.CasResult
 import com.raycoarana.memkched.internal.result.GetGatResult
+import com.raycoarana.memkched.internal.result.GetsGatsResult.Value
 import com.raycoarana.memkched.internal.result.SetResult
 import com.raycoarana.memkched.test.Containers
 import com.raycoarana.memkched.test.StringToBytesTranscoder
@@ -50,6 +54,65 @@ class MemkchedBinaryIntegrationTest {
                 ),
                 multiGetResult
             )
+        }
+    }
+
+    @Test
+    fun testCasE2E() {
+        val client = MemkchedClientBuilder()
+            .node(InetSocketAddress(memcached.host, memcached.getMappedPort(11211)))
+            .protocol(BINARY)
+            .operationTimeout(1, DAYS)
+            .build()
+
+        runBlocking {
+            client.initialize()
+
+            val casResult =
+                client.cas("some-key", "some-data", StringToBytesTranscoder, Relative(100), CasUnique(123))
+            assertEquals(CasResult.NotFound, casResult)
+
+            val result = client.set("some-key", "HELLO", StringToBytesTranscoder, Relative(100))
+            assertEquals(SetResult.Stored, result)
+
+            val casUnique = when (val getsResult = client.gets("some-key", StringToBytesTranscoder)) {
+                is Value -> getsResult.casUnique
+                else -> error("Unexpected response")
+            }
+
+            val otherCasUnique = CasUnique(casUnique.value + 1)
+            val casWithNonMatchingUniqueResult =
+                client.cas("some-key", "some-data", StringToBytesTranscoder, Relative(100), otherCasUnique)
+            assertEquals(CasResult.Exists, casWithNonMatchingUniqueResult)
+
+            val casWithMatchingUniqueResult =
+                client.cas("some-key", "some-data", StringToBytesTranscoder, Relative(100), casUnique)
+            assertEquals(CasResult.Stored, casWithMatchingUniqueResult)
+        }
+    }
+
+    @Test
+    fun testAddReplaceE2E() {
+        val client = MemkchedClientBuilder()
+            .node(InetSocketAddress(memcached.host, memcached.getMappedPort(11211)))
+            .protocol(BINARY)
+            .operationTimeout(1, DAYS)
+            .build()
+
+        runBlocking {
+            client.initialize()
+
+            val replaceNotExistsResult = client.replace("some-key", "some-data", StringToBytesTranscoder, Relative(100))
+            assertEquals(AddReplaceResult.NotStored, replaceNotExistsResult)
+
+            val addResult = client.add("some-key", "some-data", StringToBytesTranscoder, Relative(100))
+            assertEquals(AddReplaceResult.Stored, addResult)
+
+            val replaceResult = client.replace("some-key", "some-data", StringToBytesTranscoder, Relative(100))
+            assertEquals(AddReplaceResult.Stored, replaceResult)
+
+            val addExistsResult = client.add("some-key", "some-data", StringToBytesTranscoder, Relative(100))
+            assertEquals(AddReplaceResult.NotStored, addExistsResult)
         }
     }
 }
